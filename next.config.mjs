54b87097941a7e06e329a13c5e93dd5966135ee5
karimkhaled0 <kkmawe@gmail.com>
2024-07-23 20:15:48 +0300

    chore: Update image import and configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,6 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.aceternity.com',
-      },
-    ],
-  },
 }
 
 const withMDX = nextMDX({
